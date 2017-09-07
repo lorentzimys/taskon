ddbdba20f4952acd3f81bdbeb3f7be89d2c873c5
Alexey Stepanov <lorentzimys@yandex.ru>
2017-09-07 22:40:20 +0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+'use strict';
+
+let webpack = require('webpack');
+let autoprefixer = require('autoprefixer');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let ENV = process.env.npm_lifecycle_event;
+let isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+
+    let config = {};
+
+    config.entry = {
+        app: './src/app/app.js'
+    };
+
+    config.output = {
+        // Absolute output directory
+        path: __dirname + '/dist',
+
+        // Output path from the view of the page
+        // Uses webpack-dev-server in development
+        publicPath: isProd ? '/' : 'http://localhost:8080/',
+
+        // Filename for entry points
+        // Only adds hash in build mode
+        filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
+
+        // Filename for non-entry points
+        // Only adds hash in build mode
+        chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
+    };
+
+    if (isProd) {
+        config.devtool = 'source-map';
+    }
+    else {
+        config.devtool = 'eval-source-map';
+    }
+
+    config.module = {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style-loader',
+                    loader: [
+                        {loader: 'css-loader', query: {sourceMap: true}},
+                        {loader: 'postcss-loader'}
+                    ],
+                })
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+        ]
+    };
+
+    config.plugins = [
+        new webpack.LoaderOptionsPlugin({
+            test: /\.scss$/i,
+            options: {
+                postcss: {
+                    plugins: [autoprefixer]
+                }
+            }
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html',
+            inject: 'body'
+        }),
+
+        new ExtractTextPlugin({filename: 'css/[name].css', disable: !isProd, allChunks: true})
+    ];
+
+    if (isProd) {
+        config.plugins.push(
+            new webpack.NoErrorsPlugin(),
+
+            new webpack.optimize.DedupePlugin(),
+
+            new webpack.optimize.UglifyJsPlugin(),
+
+            new CopyWebpackPlugin([{
+                from: __dirname + '/src/public'
+            }])
+        )
+    }
+
+    config.devServer = {
+        contentBase: './src/public',
+        stats: 'minimal'
+    };
+
+    return config;
+}();
