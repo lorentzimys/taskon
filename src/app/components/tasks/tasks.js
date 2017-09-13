6e4dde35ba83662c1fb70e7d8c6387ed35c6c2d2
Alexey Stepanov <lorentzimys@yandex.ru>
2017-09-13 19:26:39 +0400

    Fixed staged animation

--- src/app/components/tasks/tasks.js
+++ src/app/components/tasks/tasks.js
@@ -42,7 +42,6 @@ export class TasksCtrl {
         };
 
         this.togglePropCheckbox = (prop, e) => {
-
             // Если у target имеется value - значит это input
             if (e.target.value) {
                 const propType = prop.type;
@@ -50,19 +49,13 @@ export class TasksCtrl {
                 (this.taskPropertyCollection.hasOwnProperty(propType)) ?
                     delete this.taskPropertyCollection[propType] :
                     this.taskPropertyCollection[propType] =  null;
-
-                console.log(this.taskPropertyCollection);
             }
-
         };
 
         this.filterTasks = (t, i, a) => {
             let taskPropObj = {};
 
-
             t.taskProp.map(p => taskPropObj[p] = null);
-
-            console.log(this.taskPropertyCollection === taskPropObj);
 
             return !(
                 (JSON.stringify(this.taskPropertyCollection) !== JSON.stringify(taskPropObj)) ||
@@ -77,50 +70,14 @@ export class TasksCtrl {
 
         this.taskPropertyCollection = {};
 
-
-        // this.tasksOfTypeCount = taskTypeNames.map((typeName) => {
-        //     return {
-        //         typeName: this.taskService.getTasksByType(typeName).length;
-        //     }
-        // });
-
         this.tasks = this.taskService.getAllTasks();
 
         this.taskProps = this.taskService.getTasksProperties();
-
-
-        console.log(taskTypeNames);
-        console.log(this.tasks);
     }
 }
 
 angular.module(MODULE_NAME, [tabMenu, filterPanel])
     .directive('tasks', tasks)
-    .controller('TasksCtrl', TasksCtrl)
-    .animation('.task-row', ['$animateCss', ($animateCss) => {
-        return {
-            enter: (el, done) => {
-                let animation = $animateCss(el, {
-                    easing: 'ease-in',
-                    from: { opacity:'0' },
-                    to: { opacity: '1' },
-                    duration: 0.5,
-                    delay: 0
-                });
-
-                animation.start();
-            },
-            leave: (el, done) => {
-                let animation = $animateCss(el, {
-                    easing: 'ease-out',
-                    from: { opacity:'1' },
-                    to: { opacity: '0' },
-                    duration: 0.3
-                });
-
-                animation.start();
-            }
-        }
-    }]);
+    .controller('TasksCtrl', TasksCtrl);
 
 export default MODULE_NAME;
